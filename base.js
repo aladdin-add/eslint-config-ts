fb256f0ad275786a8f98ae26cba62597b38a46f7
weiran.zsd <weiran.zsd@alibaba-inc.com>
2018-07-18 04:03:23 +0800

    Breaking: ecmaVersion defaults to ES9

--- base.js
+++ base.js
@@ -4,6 +4,7 @@ const base = [
   './lib/legacy',
   './lib/rules/es6',
   './lib/rules/es8',
+  './lib/rules/es9',
 ].map(require.resolve);
 
 module.exports = {
--- react.js
+++ react.js
@@ -5,6 +5,7 @@ module.exports = {
     './lib/legacy',
     './lib/rules/es6',
     './lib/rules/es8',
+    './lib/rules/es9',
     './lib/rules/browser',
     './lib/rules/import',
     './lib/rules/react',
