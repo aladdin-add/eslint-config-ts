f51d9407c0c33abef9a93f69b849b9c23a608721
weiran.zsd <weiran.zsd@alibaba-inc.com>
2019-01-03 16:11:45 +0800

    Breaking: switch to eslint-plugin-typescript/recommended

--- node.js
+++ node.js
@@ -4,9 +4,5 @@ module.exports = {
   extends: [
     require.resolve('./base'),
     'plugin:node/recommended',
-    'plugin:typescript/recommended',
   ],
-  rules: {
-    "typescript/indent": [2, 2]
-  }
 };
